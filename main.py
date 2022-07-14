b8ba1f7c04aa90c9c57f8ac986e038f26b86dd72
Jeremy Chow <27985157+jeremyjevon@users.noreply.github.com>
2022-07-14 13:24:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,19 +30,19 @@ def process():
 	if 'checkpot' in detectorArray: 
 		os_cmd1 = os.chdir("../checkpot")
 		os_cmd2 = os.popen("python3 checkpot.py -t " + ip_address + " -l 3").read()
-	elif 'honeybee' in detectorArray: 
+	if 'honeybee' in detectorArray: 
 		os_cmd3 = os.chdir("../honeybee")
 		os_cmd4 = os.popen("python3 glastopf.py " + ip_address).read()
-	elif 'detect-kippo-cowrie' in detectorArray: 
+	if 'detect-kippo-cowrie' in detectorArray: 
 		os_cmd5 = os.chdir("../detect-kippo-cowrie")
 		os_cmd6 = os.popen("python3 detectKippoCowrie.py " + ip_address + " 22").read()
-	elif 'honeydetect' in detectorArray: 
+	if 'honeydetect' in detectorArray: 
 		os_cmd7 = os.chdir("../honeydetect")
 		with open("addresses.txt", "w+") as f:
 			f.write(ip_address)
 			f.close()
 		os_cmd8 = os.popen("./honeydetect").read()
-	elif 'yogi' in detectorArray: 
+	if 'yogi' in detectorArray: 
 		os_cmd9 = os.chdir("../yogi")
 		with open("host_list.txt", "w+") as f:
 			f.write(ip_address)
