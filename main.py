dd2a343ea5d81254c82af494e622dcafad34d2f7
ubuntu <jeremyjevonchow@live.com>
2022-07-18 22:29:16 +0800

    updated log output to individual files

--- main.py
+++ main.py
@@ -11,26 +11,50 @@ def index():
 
 @app.route('/log')
 def log():
-	logging = []
 	log_ip = []
 	log_detectors = []
+	log_checkpot = []
+	log_honeybee = []
+	log_detectkippocowrie = []
+	log_honeydetect = []
+	log_yogi = []
 
-	with open("output.txt", "r") as f:
-	    for line in f.readlines():
-	        logging.append(line.rstrip())
-	    f.close()
-
-	with open("ip.txt", "r") as f:
+	with open("log_ip.txt", "r") as f:
 	    for line in f.readlines():
 	        log_ip.append(line.rstrip())
 	    f.close()
 
-	with open("detectors.txt", "r") as f:
+	with open("log_detectors.txt", "r") as f:
 	    for line in f.readlines():
 	        log_detectors.append(line.rstrip())
 	    f.close()
 
-	return render_template("log.html", logging=logging, log_ip=log_ip, log_detectors=log_detectors)
+	with open("log_checkpot.txt", "r") as f:
+	    for line in f.readlines():
+	        log_checkpot.append(line.rstrip())
+	    f.close()
+
+	with open("log_honeybee.txt", "r") as f:
+	    for line in f.readlines():
+	        log_honeybee.append(line.rstrip())
+	    f.close()
+
+	with open("log_detectkippocowrie.txt", "r") as f:
+	    for line in f.readlines():
+	        log_detectkippocowrie.append(line.rstrip())
+	    f.close()
+
+	with open("log_honeydetect.txt", "r") as f:
+	    for line in f.readlines():
+	        log_honeydetect.append(line.rstrip())
+	    f.close()
+
+	with open("log_yogi.txt", "r") as f:
+	    for line in f.readlines():
+	        log_yogi.append(line.rstrip())
+	    f.close()
+
+	return render_template("log.html", log_ip=log_ip, log_detectors=log_detectors, log_checkpot=log_checkpot, log_honeybee=log_honeybee, log_detectkippocowrie=log_detectkippocowrie, log_honeydetect=log_honeydetect, log_yogi=log_yogi)
 
 @app.route('/about')
 def about():
@@ -46,47 +70,58 @@ def process():
 	detectorArray = request.form.getlist('check')
 	detectors = (", ".join(detectorArray))
 	
-	os_cmd3 = os_cmd5 = os_cmd7 = os_cmd9 = os_cmd11 = " "
+	os_cmd1 = os_cmd2 = os_cmd3 = os_cmd4 = os_cmd5 = " "
 
 	if 'checkpot' in detectorArray: 
-		os_cmd2 = os.chdir("../checkpot")
-		os_cmd3 = os.popen("python3 checkpot.py -t " + ip_address + " -l 3").read()
+		os.chdir("../checkpot")
+		os_cmd1 = os.popen("python3 checkpot.py -t " + ip_address + " -l 3").read()
 	if 'honeybee' in detectorArray: 
-		os_cmd4 = os.chdir("../honeybee")
-		os_cmd5 = os.popen("python3 glastopf.py " + ip_address).read()
+		os.chdir("../honeybee")
+		os_cmd2 = os.popen("python3 glastopf.py " + ip_address).read()
 	if 'detect-kippo-cowrie' in detectorArray: 
-		os_cmd6 = os.chdir("../detect-kippo-cowrie")
-		os_cmd7 = os.popen("python3 detectKippoCowrie.py " + ip_address + " 22").read()
+		os.chdir("../detect-kippo-cowrie")
+		os_cmd3 = os.popen("python3 detectKippoCowrie.py " + ip_address + " 22").read()
 	if 'honeydetect' in detectorArray: 
-		os_cmd8 = os.chdir("../honeydetect")
+		os.chdir("../honeydetect")
 		with open("addresses.txt", "w+") as f:
 			f.write(ip_address)
 			f.close()
-		os_cmd9 = os.popen("./honeydetect").read()
+		os_cmd4 = os.popen("./honeydetect").read()
 	if 'yogi' in detectorArray: 
-		os_cmd10 = os.chdir("../yogi")
+		os.chdir("../yogi")
 		with open("host_list.txt", "w+") as f:
 			f.write(ip_address)
 			f.close()
-		os_cmd11 = os.popen("python3 yogi.py").read()
+		os_cmd5 = os.popen("python3 yogi.py").read()
 
-	os_cmd0 = os.chdir("../ICT3211-T-Pot-Detector")
-	with open("ip.txt", "w+") as f:
+	os.chdir("../ICT3211-T-Pot-Detector")
+
+	with open("log_ip.txt", "w+") as f:
 		f.write(ip_address)
 		f.close()
 
-	os_cmd1 = os.chdir("../ICT3211-T-Pot-Detector")
-	with open("detectors.txt", "w+") as f:
+	with open("log_detectors.txt", "w+") as f:
 		f.write(detectors)
 		f.close()
 
-	os_cmd0 = os.chdir("../ICT3211-T-Pot-Detector")
-	with open("output.txt", "w+") as f:
+	with open("log_checkpot.txt", "w+") as f:
+		f.write(os_cmd1)
+		f.close()
+
+	with open("log_honeybee.txt", "w+") as f:
+		f.write(os_cmd2)
+		f.close()
+
+	with open("log_detectkippocowrie.txt", "w+") as f:
 		f.write(os_cmd3)
+		f.close()
+
+	with open("log_honeydetect.txt", "w+") as f:
+		f.write(os_cmd4)
+		f.close()
+
+	with open("log_yogi.txt", "w+") as f:
 		f.write(os_cmd5)
-		f.write(os_cmd7)
-		f.write(os_cmd9)
-		f.write(os_cmd11)
 		f.close()
 
 	return render_template("process.html")
